84e6f67002a87835b5e4390c5f3e0d5ab56a8dea
Aaron Wilson <codingsensei19@gmail.com>
2020-02-11 01:26:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,20 @@
 class Dog
-  attr_accessor :mood
-  attr_reader :name
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
-    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
   end
 end
